22fd6f39722ca1f03c3377ddd92f3fb896a0b757
HR0N <vivante.des@gmail.com>
2022-07-01 15:03:40 +0300

    add contrcts server logic

--- wp-content/themes/underscores/assets/models/contacts.php
+++ wp-content/themes/underscores/assets/models/contacts.php
@@ -14,3 +14,23 @@ function marketing_offer_3(){
     $chargers_diapason = [1.8, 2.13];
     $limitation = 7;
 }
+function marketing_offer_11(){
+    $minmax_deposit = [1, 99];
+    $chargers_diapason = .5;
+}
+function marketing_offer_12(){
+    $minmax_deposit = [100, 499];
+    $chargers_diapason = .7;
+}
+function marketing_offer_13(){
+    $minmax_deposit = [500, 999];
+    $chargers_diapason = .9;
+}
+function marketing_offer_14(){
+    $minmax_deposit = [1000, 4999];
+    $chargers_diapason = 1.1;
+}
+function marketing_offer_15(){
+    $minmax_deposit = [5000, 9999999];
+    $chargers_diapason = 1.3;
+}
--- wp-content/themes/underscores/assets/php/h_get_bills.php
+++ wp-content/themes/underscores/assets/php/h_get_bills.php
@@ -1,2 +1,2 @@
 <?php
-return connectDB("SELECT * FROM `wp_user_bills`");
+//return connectDB("SELECT * FROM `wp_user_bills`");
